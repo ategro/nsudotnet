cd0711868bd646c1d4dd5dd7d20e42e2e5f74a1f
Sklyarov Mikhail <ategro@yandex.ru>
2016-05-25 11:54:09 +0600

    Enigma: #1 & #2 - исправлено

--- Sklyarov.Nsudotnet.Enigma/Sklyarov.Nsudotnet.Enigma/Enigma.cs
+++ Sklyarov.Nsudotnet.Enigma/Sklyarov.Nsudotnet.Enigma/Enigma.cs
@@ -9,81 +9,71 @@ namespace Sklyarov.Nsudotnet.Enigma
         public static void Encrypt(String inName,  String algoName, String outName)
         {
             SymmetricAlgorithm algo = SymmetricAlgorithm.Create(algoName);
-            BaseAlgorithm(inName, outName, algo.CreateEncryptor());
             CreateKeyFile(inName, algo);
+            BaseAlgorithm(inName, outName, algo.CreateEncryptor());
         }
 
         public static void Decrypt(String inName, String algoName, String keyName, String outName)
         {
             SymmetricAlgorithm algo = SymmetricAlgorithm.Create(algoName);
-            StreamReader keyStream = new StreamReader(keyName);
-
-            string key = keyStream.ReadLine();
-            string iV = keyStream.ReadLine();
-
-            algo.Key = Convert.FromBase64String(key);
-            algo.IV = Convert.FromBase64String(iV);
-
+            LoadKeyFile(keyName, algo);
             BaseAlgorithm(inName, outName, algo.CreateDecryptor());
         }
 
         private static void BaseAlgorithm(String inName, String outName, ICryptoTransform cryptoTransform)
         {
-            FileStream inStream = new FileStream(inName, FileMode.Open, FileAccess.Read);
-            FileStream outStream = new FileStream(outName, FileMode.OpenOrCreate, FileAccess.Write);
-            outStream.SetLength(0);
-
-            CryptoStream cryptoStream = new CryptoStream(outStream, cryptoTransform, CryptoStreamMode.Write);
-
-            Write(inStream, cryptoStream);
-
-            cryptoStream.Close();
-            outStream.Close();
-            inStream.Close();
-        }
-
-        private static void Write(FileStream fileStream, CryptoStream cryptoStream)
-        {
-            byte[] buffer = new byte[4096];
-            long readLen = 0;
-            long totalLen = fileStream.Length;
-            int len;
-            byte totalPercent = 0;
-
-            while (readLen < totalLen)
+            using (FileStream inStream = new FileStream(inName, FileMode.Open, FileAccess.Read),
+                              outStream = new FileStream(outName, FileMode.OpenOrCreate, FileAccess.Write))
             {
-                len = fileStream.Read(buffer, 0, buffer.Length);
-                cryptoStream.Write(buffer, 0, len);
-                readLen = readLen + len;
-                byte temp = (byte)(100.0 * readLen / totalLen);
-                if (temp != totalPercent)
+                outStream.SetLength(0);
+
+                using (CryptoStream cryptoStream = new CryptoStream(outStream, cryptoTransform, CryptoStreamMode.Write))
                 {
-                    totalPercent = temp;
-                    Console.SetCursorPosition(0, 0);
-                    Console.WriteLine("{0}% processed.", totalPercent);
+                    Write(inStream, cryptoStream);
                 }
             }
         }
 
+        private static void Write(FileStream fileStream, CryptoStream cryptoStream)
+        {
+            fileStream.CopyTo(cryptoStream);
+        }
+
         private static void CreateKeyFile(String fileName, SymmetricAlgorithm algo)
         {
+            string keyName = GetKeyName(fileName);
+
+            using (StreamWriter keyStream = new StreamWriter(keyName))
+            {
+                string key = Convert.ToBase64String(algo.Key);
+                string iV = Convert.ToBase64String(algo.IV);
+
+                keyStream.WriteLine(key);
+                keyStream.WriteLine(iV);
+            }
+        }
+
+        private static void LoadKeyFile(String fileName, SymmetricAlgorithm algo)
+        {
+            using (StreamReader keyStream = new StreamReader(fileName)) 
+            {
+                string key = keyStream.ReadLine();
+                string iV = keyStream.ReadLine();
+
+                algo.Key = Convert.FromBase64String(key);
+                algo.IV = Convert.FromBase64String(iV); 
+            }
+        }
+
+        private static string GetKeyName(String fileName) 
+        {
             int pos = fileName.LastIndexOf('.');
-            string keyName;
             if (pos == -1)
             {
                 pos = fileName.Length;
             }
-            keyName = string.Format("{0}.key{1}", fileName.Substring(0, pos), fileName.Substring(pos));
 
-            StreamWriter keyStream = new StreamWriter(keyName);
-            
-            string key = Convert.ToBase64String(algo.Key);
-            string iV = Convert.ToBase64String(algo.IV);
-            
-            keyStream.WriteLine(key);
-            keyStream.WriteLine(iV);
-
-            keyStream.Close();
+            return string.Format("{0}.key{1}", fileName.Substring(0, pos), fileName.Substring(pos));
         }
     }
 }
